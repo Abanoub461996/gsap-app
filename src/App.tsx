6e1f4cc83c5d0ec1ea90468f6c33e47e5312247c
Abanoub461996 <pophtc.aa@gmail.com>
2024-03-14 13:01:58 +0200

    config: adding tailwind and starting with simple gsap animation on scrolltrigger

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const container = useRef<HTMLDivElement>(null);
+  const circle = useRef<HTMLDivElement>(null);
+  gsap.registerPlugin(ScrollTrigger);
+
+  useGSAP(
+    () => {
+      // use selectors...
+      gsap.to(".box", {
+        scrollTrigger: {
+          trigger: ".box", // Set the trigger element
+          start: "top center", // Adjust the start position as needed
+          end: "bottom center", // Adjust the end position as needed
+          scrub: true, // Smoothly animates values between scroll positions
+          once: false // Ensure the animation is repeated
+        },
+        x: 500 // Animation properties
+      });
+    },
+    // { scope: container }
+  ); // <-- scope for selector text (optional)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App h-[300vh]">
+      <div ref={container} className="container">
+        <div className="bg-black h-[150vh]">selector</div>
+        <div className="bg-red rounded-[50%] gradient-green" ref={circle}>
+          Ref
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div className="box h-64 w-8 bg-blue">box</div>
+    </div>
+  );
 }
 
-export default App
+export default App;
