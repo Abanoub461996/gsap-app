8c3138c169ba75201afa9fd5d01e49e767fb9f4d
Abanoub461996 <pophtc.aa@gmail.com>
2024-03-15 13:04:38 +0200

    add: parallax section with scrolltrigger

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
 import HorizontalScroll from "./elements/scrollTrigger/HorizontalScroll";
-import Parallax from "./elements/scrollTrigger/Parallax";
+import ParallaxHeader from "./elements/scrollTrigger/ParallaxHeader";
 
 import "./App.css";
+import ParallaxSection from "./elements/scrollTrigger/ParallaxSection";
+
+
 function App() {
   return (
     <>
-      <div className="skill">
-        <div className="h-32 w-full flex justify-center items-center font-black">
+      <div className="skill relative">
+        <div className="absolute top-4 right-[50%] translate-x-[50%] text-black font-black">
           <h1>Parallax Coming</h1>
         </div>
 
-        <Parallax />
+        <ParallaxHeader />
       </div>
-      <div className="skill">
-        <div className="h-32 w-full flex justify-center items-center font-black">
+      <div className="skill relative bg-[#130d0a] bg-wheat">
+        <div className="absolute top-4 right-[50%] translate-x-[50%] z-10 font-black">
+          <h1>Parallax Section Coming</h1>
+        </div>
+
+        <ParallaxSection/>
+      </div>
+      <div className="skill relative">
+        <div className="absolute top-4 right-[50%] translate-x-[50%] z-10 font-black">
           <h1>Horizontal Scroll Coming</h1>
         </div>
 
         <HorizontalScroll />
       </div>
+      
     </>
   );
 }
